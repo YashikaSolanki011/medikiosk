19101a480109a6dc9575e47a6cecfe20a55076e5
YashikaSolanki011 <solankiyaashika@gmail.com>
2026-09-23 08:57:50 +0530

    Delete deploy_local.sh

--- deploy_local.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-echo "=== Starting MediKiosk Locally ==="
-pip install -r requirements.txt
-export PORT=7860
-python3 -m uvicorn backend.main:app --host 0.0.0.0 --port 7860 --reload
